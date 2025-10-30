16862dcd38fa968bf8ff621b00d92e64a474e383
Xihang Chen <143648286+chen-xihang@users.noreply.github.com>
2025-10-30 18:46:08 +0000

    v0

--- time_series/ARIMA.R
+++ time_series/ARIMA.R
@@ -1,2 +1,20 @@
 ### Using ARIMA to forecast commodity prices
-library(fable)
+library(fpp3)
+library(quantmod)
+library(tidyverse)
+library(fabletools)
+
+# calling historical prices
+getSymbols("BZ=F", src = "yahoo", from = "2008-01-01", to = Sys.Date())
+brent <- `BZ=F`[, "BZ=F.Close"]
+plot(brent, major.ticks = "year")
+
+brent_df <- tibble(
+  date = index(brent),
+  price = as.numeric(brent)
+)
+
+brent_tsbl <- as_tsibble(brent_df, index = date)
+
+training_data_brent <- brent_tsbl |>
+  dplyr::filter(year(date)<=2022)
