3f0ead7c7c05dec4ebc3b331d1404aa8a83c8002
Xihang Chen <143648286+chen-xihang@users.noreply.github.com>
2025-11-02 13:14:54 +0000

    Update ARIMA.R

--- time_series/ARIMA.R
+++ time_series/ARIMA.R
@@ -20,6 +20,24 @@ brent_tsbl <- tibble(
   fill_gaps(date = seq.Date(min(date), max(date), by = "day")) |>
   dplyr::mutate(price = na_interpolation(price, option = "linear"))
 
+test <- tibble(
+  date = index(brent),
+  price = as.numeric(brent)
+) |>
+  dplyr::filter(year(date)>2022, year(date)<=2024) |>
+  bind_rows(
+    tibble(
+      date  = as.Date(c("2022-12-31", "2023-01-01","2023-01-02","2023-01-03")), # forward fill
+      price = c(84.33, 83.260, 82.26, 85.91)
+    )
+  ) |>
+  mutate(date = as.Date(date)) |>
+  arrange(date) |>
+  distinct(date, .keep_all = TRUE) |>
+  as_tsibble(index = date) |>
+  fill_gaps(date = seq.Date(min(date), max(date), by = "day")) |>
+  dplyr::mutate(price = na_interpolation(price, option = "linear"))
+
 brent_tsbl |> autoplot(price) + labs(title = "Brent Price Before 2022") 
 
 # testing stationarity
@@ -36,4 +54,30 @@ d1_brent |> autoplot(price_diff) + labs(title = "Price Difference Before 2022")
 d1_brent |> ACF(price_diff) |> autoplot() + labs(title = "ACF of Price Difference Before 2022") 
 d1_brent |> features(price_diff, unitroot_kpss) # d = 1
 
+# ARIMA with d = 1
+fit <- brent_tsbl %>%
+  model(arima_fixed_d = ARIMA(price ~ pdq(d = 1)))
 
+brent_tsbl %>% 
+  model(arima = ARIMA(price)) # gives the same ARIMA model, minimises AICc
+
+fit %>% gg_tsresiduals() # plotting residues
+
+fit %>%
+  augment() %>%
+  autoplot(.fitted, colour = "green") +   # predicted (fitted) = black
+  autolayer(brent_tsbl, price, colour = "blue") +  # actual = red
+  labs(title = "ARIMA Fitted vs Actual (Training Set)",
+       y = "Price", x = "Time")
+
+# checking with test data set
+fit_2y <- fit |> forecast(h = "2 years")
+fit_2y_test <- fit |> forecast(new_data = test)
+
+autoplot(fit_2y, brent_tsbl) +
+  labs(title = "ARIMA Forecast: 2 Years Ahead",
+       y = "Price", x = "Time")
+
+autoplot(fit_2y_test, brent_tsbl) +
+  labs(title = "ARIMA Forecast: 2 Years Ahead",
+       y = "Price", x = "Time")
